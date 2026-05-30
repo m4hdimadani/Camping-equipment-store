852d0a08b1b5a889aa90b585765758cdd4a78c0e
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2026-05-30 16:01:31 +0330

    updating and created best products

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-
-
 // اسکرول نوبار
 let lastScrollTop = 0;
-const navbar = document.getElementById('main-navbar');
+const navbar = document.getElementById("main-navbar");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > lastScrollTop && scrollTop > 50) {
-    navbar.classList.add('navbar-hidden');
+    navbar.classList.add("navbar-hidden");
   } else {
-    navbar.classList.remove('navbar-hidden');
+    navbar.classList.remove("navbar-hidden");
   }
-  
+
   lastScrollTop = scrollTop;
 });
 // اسکرول نوبار
+
+// swapir محصولات پر فروش
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 4,
+});
+// swapir محصولات پر فروش
